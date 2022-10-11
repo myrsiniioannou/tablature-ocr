326e4f4d28249713ca1d488c3dc946fb26ff1277
Myrsini Ioannou <mersene.io@gmail.com>
2022-10-11 17:47:48 +0300

    still working on template rendering dear god

--- modules/book_renderer.py
+++ modules/book_renderer.py
@@ -3,6 +3,17 @@ from domain_model import *
 import json
 from jinja2 import Environment, FileSystemLoader
 
+def setParagraphPagesAndHeadings():
+    paragraphs = {
+        "IA":[1,4],
+        "IB": [3,5]
+    }
+    headings = {
+        "doubleHeading": [*range(1,8), *range(18,30)],
+        "singleHeading": [*range(8,18), *range(30,40)]
+    }
+    return paragraphs, headings
+
 
 def render(JSON):
     # Load JSON BOOK FILE
@@ -11,24 +22,28 @@ def render(JSON):
     file_loader = FileSystemLoader("templates")
     env = Environment(loader = file_loader)
 
+    paragraphs, headings = setParagraphPagesAndHeadings()
+
+
+    # ask for I Paragraph
+
+
+
 
     # FTIAKSE TO DICT TOU DATA SCTRUCTURE STH MORFH POY PREPEI GIA NA RENTAREIS META
-    # DLD KANE TOUS YPOLOGISMOUS POU PREPEI
 
-    #eatShit = env.get_template("content.mscx").render()
 
-    render = env.get_template("base.mscx").render(
-        NumberOfStrings = bookInJsonFormat["numberofstrings"],
-        dataaa = 'CONTENT',
-        variab = "Hello to you too")
 
-    print(render)
+    #render = env.get_template("base.mscx").render(
+    #    NumberOfStrings = bookInJsonFormat["numberofstrings"],
+    #    dataaa = 'CONTENT',
+    #    variab = "Hello to you too")
 
-#"""
-#musescoreOutputFile = r"C:\Users\merse\Desktop\Tablature OCR\final_musescore_outputs\book.mscx"
-##with open(f"{musescoreOutputFile}", "w") as f:
-#    f.write(render)
-#"""
+    #print(render)
+
+    #musescoreOutputFile = r"C:\Users\merse\Desktop\Tablature OCR\final_musescore_outputs\book.mscx"
+    ##with open(f"{musescoreOutputFile}", "w") as f:
+    #    f.write(render)
 
 
 
